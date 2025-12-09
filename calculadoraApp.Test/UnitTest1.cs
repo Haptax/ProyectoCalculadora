f2331e79622576a225a260146a9b06dc98b3ff63
Xavier <xavoledesma@gmail.com>
2025-12-09 11:04:08 -0500

    Proyecto Calculadora

---
+++ calculadoraApp.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+namespace calculadoraApp.Test
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrecta()
+        {
+            var calc = new calculadoraApp.calculadora();
+            var resultado = calc.Sumar(2, 3);
+            Assert.Equal(5, resultado);
+        }
+    }
+}
+
+
